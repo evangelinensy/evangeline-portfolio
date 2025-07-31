7599472ce281a903185ed34d1506d1686f107f39
Evangeline Ng <eggsvans@gmail.com>
2025-07-31 01:26:40 -0700

    Update biography to include current role as Staff UX Designer at Palo Alto Networks

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -26,8 +26,8 @@ export default function AboutPage() {
                   Hello, I&apos;m Evangeline Ng
                 </h2>
                 <p className="mb-6 text-base text-muted-foreground md:text-lg">
-                  I’m a Singaporean-born product designer, based in San Francisco.<br /><br />
-                  Over the years, I’ve been delivering impact to early-stage startups by leading the product and improving the businesses through design.<br /><br />
+                  I&apos;m a Singaporean-born product designer, based in San Francisco. Currently, a Staff UX Designer at Palo Alto Networks.<br /><br />
+                  Over the years, I&apos;ve been delivering impact to early-stage startups by leading the product and improving the businesses through design.<br /><br />
                   Previously at DeForm I helped brands reward their community and go onchain at DeForm as their Founding Product Designer.<br /><br />
                   At Pomelo, I worked on helping immigrants like myself to remit money fee-free, on credit, as their Second Product Designer.<br /><br />
                   I helped clinicians work efficiently at a Medtech Startup called See-mode. I was their first design hire and built the design foundations of their ultrasound web application.<br /><br />
