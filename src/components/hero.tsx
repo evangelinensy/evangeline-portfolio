e3d3975f8bf076ae3a01f901e0cbd07b73c96b6b
Evangeline Ng <eggsvans@gmail.com>
2025-09-08 14:53:41 -0700

    feat: change LLMCook subtitle to 'Game'

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -66,7 +66,7 @@ export function Hero() {
     ) : undefined,
               discSrc: i === 1 ? '/images/disc-sanrio.png' : i === 2 ? '/images/disc-cromatic-new.png' : i === 3 ? '/images/disc-egg.png' : i === 4 ? '/images/disc-llmcook.png' : undefined,
     sleeveTitle: i === 1 ? 'Sanrio' : i === 2 ? 'Cromatic' : i === 3 ? 'About Me' : i === 4 ? 'LLMCook' : undefined,
-    sleeveSubtitle: i === 1 ? 'Web3 Campaigns' : i === 2 ? 'TechBio' : i === 3 ? 'My experiences' : i === 4 ? 'Game to learn LLM' : undefined,
+    sleeveSubtitle: i === 1 ? 'Web3 Campaigns' : i === 2 ? 'TechBio' : i === 3 ? 'My experiences' : i === 4 ? 'Game' : undefined,
     sleeveBottomCaption: i === 1 ? 'Web App' : i === 2 ? 'Desktop Web App' : i === 3 ? 'Slides' : i === 4 ? 'Web App' : undefined,
   }));
 
